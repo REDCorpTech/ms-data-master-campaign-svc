f4fe7331e87e35c4736ebc08bd2d73ecdeb1b387
nocturnalcoders <christiantolie99@gmail.com>
2025-12-22 00:35:39 +0800

    adding new lib

---
+++ src/main/resources/db/migration/V1.1.5__CREATE_INDEX_CAMPAIGN_AND_CAMPAIGN_LOG.sql
@@ -0,0 +1,10 @@
+CREATE INDEX idx_campaign_product_details_gin
+    ON t_campaign
+    USING GIN (product_details);
+
+CREATE INDEX idx_scan_log_product_details_gin
+    ON t_campaign_scan_log
+    USING GIN (product_details);
+
+CREATE INDEX idx_scan_log_email
+    ON t_campaign_scan_log(email);
