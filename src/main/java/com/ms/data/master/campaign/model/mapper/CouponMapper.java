45cb7575dfb97ef22d7b8e2ecaf2ec5b4abb45b3
nocturnalcoders <christiantolie99@gmail.com>
2026-01-21 03:15:40 +0800

    change yml and mapper

--- src/main/java/com/ms/data/master/campaign/model/mapper/CouponMapper.java
+++ src/main/java/com/ms/data/master/campaign/model/mapper/CouponMapper.java
@@ -1,11 +1,8 @@
 package com.ms.data.master.campaign.model.mapper;
 
-import com.ms.data.master.campaign.model.Campaign;
 import com.ms.data.master.campaign.model.Coupon;
-import com.ms.data.master.campaign.model.dto.campaign.CampaignDTO;
 import com.ms.data.master.campaign.model.dto.campaign.CouponDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
